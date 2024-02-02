af6165683502184043ec3ba639a420304636751c
lusk <lucas78654321@outlook.com>
2024-02-02 19:56:35 -0300

    :tada: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
+export function App() {
+  return (
+    <div className='bg-slate-400 min-h-screen'>
+      <Button variant={'destructive'}>
+        asdas
+      </Button>
+    </div>
+  )
+}
