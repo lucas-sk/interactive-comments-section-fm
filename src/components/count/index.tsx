4ce5310b674899657ea7089c63079a8cfa09d37f
lusk <lucas78654321@outlook.com>
2024-02-03 15:03:10 -0300

    feat: added interaction with buttons

--- src/components/count/index.tsx
+++ src/components/count/index.tsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import { Minus } from "../icons/Minus";
 import { Plus } from "../icons/Plus";
 import { Button } from "../ui/button";
 
 export function Count(){
+  const [count, setCount] = useState(0);
+
+  function handleIncrement(){
+    setCount((state) => {
+      return state + 1;
+    })
+  }
+
+  function handleDecrement(){
+    setCount((state) => {
+      return state - 1;
+    })
+  }
+
   return (
     <div className="flex items-center bg-flash-500 rounded-md">
-        <Button className="text-periwinkle hover:text-iris" size={"icon"} variant={"outline"}>
+        <Button onClick={handleIncrement} className="text-periwinkle hover:text-iris" size={"icon"} variant={"outline"}>
           <Plus />
         </Button>
-        <span className="text-iris">12</span>
-        <Button className="text-periwinkle hover:text-iris" size={"icon"} variant={"outline"}>
+        <span className="text-iris">{count}</span>
+        <Button onClick={handleDecrement} className="text-periwinkle hover:text-iris" size={"icon"} variant={"outline"}>
           <Minus />
         </Button>
     </div>
