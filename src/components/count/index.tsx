9ca14d8b1d4a1be53bdd1b91ad0a8928c62f8ed1
lusk <lucas78654321@outlook.com>
2024-02-03 15:11:54 -0300

    feat: implement increment only one and decrement only one

--- src/components/count/index.tsx
+++ src/components/count/index.tsx
@@ -3,23 +3,30 @@ import { Minus } from "../icons/Minus";
 import { Plus } from "../icons/Plus";
 import { Button } from "../ui/button";
 
-export function Count(){
-  const [count, setCount] = useState(0);
+interface CountProps {
+  stateInitial?: number;
+}
+
+export function Count({ stateInitial = 0 }: CountProps){
+  const [count, setCount] = useState(stateInitial);
 
   function handleIncrement(){
     setCount((state) => {
+      if (state  === stateInitial + 1) return state;
       return state + 1;
     })
   }
 
   function handleDecrement(){
     setCount((state) => {
+      if (state === 0) return 0;
+      if (state - 1 === stateInitial - 1) return stateInitial;
       return state - 1;
     })
   }
 
   return (
-    <div className="flex items-center bg-flash-500 rounded-md">
+    <div className="flex items-center bg-flash-500 rounded-md w-20">
         <Button onClick={handleIncrement} className="text-periwinkle hover:text-iris" size={"icon"} variant={"outline"}>
           <Plus />
         </Button>
